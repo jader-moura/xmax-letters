6e910d1e4fbb7a6261e81661a160616ddfd117be
Jader Moura <jader_vinicius_gonzaga_moura@epam.com>
2022-12-01 15:54:29 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container">
+      <main className="main">
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
